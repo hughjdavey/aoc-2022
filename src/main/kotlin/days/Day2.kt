2888ce254e0f78b495ce1e78fc6554d5f68707d0
hughjdavey <9026053+hughjdavey@users.noreply.github.com>
2022-12-02 17:15:04 +0000

    Day 2 Refactor
    Use objects to make a bit clearer than raw numbers

--- src/main/kotlin/days/Day2.kt
+++ src/main/kotlin/days/Day2.kt
@@ -1,5 +1,12 @@
 package days
 
+import days.Day2.Outcome.LOSE
+import days.Day2.Outcome.DRAW
+import days.Day2.Outcome.WIN
+import days.Day2.Shape.ROCK
+import days.Day2.Shape.PAPER
+import days.Day2.Shape.SCISSORS
+
 class Day2 : Day(2) {
 
     private val mappings = mapOf("A" to 1, "X" to 1, "B" to 2, "Y" to 2, "C" to 3, "Z" to 3)
@@ -7,37 +14,37 @@ class Day2 : Day(2) {
     private val game = inputList.map { it.split(" ").map { mappings[it]!! } }
 
     override fun partOne(): Any {
-        return game.sumOf { it.last() + getOutcome(it) }
+        return game.map { Game(Shape.values()[it[0] - 1], Shape.values()[it[1] - 1], null) }
+            .sumOf { it.getValue() }
     }
 
     override fun partTwo(): Any {
-        return game.map { getShape(it) }.sumOf { it.last() + getOutcome(it) }
+        return game.map { Game(Shape.values()[it[0] - 1], null, Outcome.values()[it[1] - 1]) }
+            .sumOf { it.getValue() }
     }
 
-    // 1 = rock, 2 = paper, 3 = scissors
-    private fun getOutcome(ns: List<Int>): Int {
-        val (them, us) = ns[0] to ns[1]
-        return when {
-            us == 1 && them == 3 -> 6
-            us == 3 && them == 2 -> 6
-            us == 2 && them == 1 -> 6
-            us == them -> 3
-            else -> 0
+    class Game(private val them: Shape, private val us: Shape?, private val outcome: Outcome?) {
+
+        fun getValue(): Int {
+            return if (us != null) getOutcome(them, us).value + us.value else getUs(them, outcome!!).value + outcome.value
+        }
+
+        private fun getOutcome(them: Shape, us: Shape): Outcome {
+            return if ((us == ROCK && them == SCISSORS) || (us == SCISSORS && them == PAPER) || (us == PAPER && them == ROCK)) WIN
+            else if (us == them) DRAW
+            else LOSE
+        }
+
+        private fun getUs(them: Shape, outcome: Outcome): Shape {
+            return when (outcome) {
+                LOSE -> when (them) { ROCK -> SCISSORS; PAPER -> ROCK; SCISSORS -> PAPER }
+                WIN -> when (them) { ROCK -> PAPER; PAPER -> SCISSORS; SCISSORS -> ROCK }
+                DRAW -> them
+            }
         }
     }
 
-    // 1 = rock, 2 = paper, 3 = scissors
-    private fun getShape(ns: List<Int>): List<Int> {
-        val (them, outcome) = ns[0] to ns[1]
-        val us = when {
-            outcome == 1 && them == 1 -> 3
-            outcome == 1 && them == 2 -> 1
-            outcome == 1 && them == 3 -> 2
-            outcome == 3 && them == 1 -> 2
-            outcome == 3 && them == 2 -> 3
-            outcome == 3 && them == 3 -> 1
-            else -> them
-        }
-        return listOf(them, us)
-    }
+    enum class Shape(val value: Int) { ROCK(1), PAPER(2), SCISSORS(3) }
+
+    enum class Outcome(val value: Int) { LOSE(0), DRAW(3), WIN(6) }
 }
